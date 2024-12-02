0062a29b9ef3c2e7ae6b2d2def20ad0f18017dbc
Ali Saeed <asaeed5637@gmail.com>
2024-12-01 17:46:13 -0800

    #1 commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "IntBinaryTree.h"
+using namespace std;
+
+void outputMenu();
+
+int main() {
+    
+
+    return 0;
+}
+
+void outputMenu() {
+    cout << "BST Menu" << endl;
+    cout << "1. \n";
+    cout << "2. \n";
+    cout << "3. \n";
+    cout << "4. \n"
+;}
