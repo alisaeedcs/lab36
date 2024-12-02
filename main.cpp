0c8dedfd27c6ca472e2e07af2919191df98b1de0
Ali Saeed <asaeed5637@gmail.com>
2024-12-01 17:56:46 -0800

    #2 commit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
 
 void outputMenu();
 
 int main() {
-    
+    IntBinaryTree tree;
+
+    ifstream input("codes.txt");
+    string line;
+
+    int choice;
+
+    do {
+        outputMenu();
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        switch (choice) {
+            case 1:
+            
+        }
+    } while (choice != 5);
+
+    input.close();
+
+
+
+
 
     return 0;
 }
 
 void outputMenu() {
     cout << "BST Menu" << endl;
-    cout << "1. \n";
-    cout << "2. \n";
-    cout << "3. \n";
-    cout << "4. \n"
-;}
+    cout << "1. Add record\n";
+    cout << "2. Delete record\n";
+    cout << "3. Search for record\n";
+    cout << "4. Output records\n";
+    cout << "5. Exit\n";
+}
